cf7e475e22dec9ca994f03f059bf036b226d9b11
Rbartio-2015 <robertoeduardohernandezbenitez@gmail.com>
2026-02-27 00:21:44 +0000

    Agregar funciones para resolver el problema de la mochila y calcular la suma de valores

--- bfrehb/__init__.py
+++ bfrehb/__init__.py
@@ -38,3 +38,19 @@ def is_3_colorable(graph):
     if is_proper_coloring(graph,coloring):
       return coloring
   return False
+
+def sum_of_values(values,keys):
+    sum=0
+    n=len(values)
+    for i in range(n):
+        sum+=values[i]*keys[i]
+    return sum
+
+def knapsap_problem(weigth,profits,capacity, goal):
+    n=len(weigth)
+    sequences = itertools.product([0, 1], repeat=n)
+    for sequence in sequences:
+        if sum_of_values(weigth,sequence)<=capacity:
+            if sum_of_values(profits,sequence)>=goal:
+                return sequence
+    return False
